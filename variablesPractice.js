5e3c1571c8522728cadcf3c00d71286b0404e458
adixit <adixit@victoria.com>
2023-05-23 14:10:52 +0530

    Day 2 - variables Practice JS

---
+++ variablesPractice.js
@@ -0,0 +1,10 @@
+/* Declare two variables: admin and myName.
+Assign the value "John" to myName.
+Copy the value from myName to admin.
+Show the value of admin using console (must output “John”). */
+
+let admin;
+let myName;
+myName = "john";
+admin = myName;
+console.log(admin);
